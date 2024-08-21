994283d0ec932509054612030b6b86fcbc92e047
andire120 <164429441+andire120@users.noreply.github.com>
2024-08-21 19:15:52 +0900

    자바2

---
+++ src/variable/ex/VarEX3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEX3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+
+        System.out.println(longVar);
+
+        boolean boolVar = true;
+
+        System.out.println(boolVar);
+    }
+}
