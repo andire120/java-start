610de4f30e276f0ebafaade3404327c09423e19f
andire120 <164429441+andire120@users.noreply.github.com>
2024-08-22 19:16:49 +0900

    자바4

---
+++ src/cond/ex/MoveRateEX.java
@@ -0,0 +1,12 @@
+package cond.ex;
+
+public class MoveRateEX {
+    public static void main(String[] args) {
+        double rating = 7.1;
+
+        if(rating <= 9) System.out.println("'파묘'을 추천합니다.");
+        if(rating <= 8) System.out.println("'노웨이홈'을 추천합니다.");
+        if(rating <= 7) System.out.println("'감청의권'을 추천합니다.");
+
+    }
+}
