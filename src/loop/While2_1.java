efd9d923a74d436f4f18a3fe441547037353d8e7
andire120 <164429441+andire120@users.noreply.github.com>
2024-08-22 19:39:42 +0900

    자바

---
+++ src/loop/While2_1.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class While2_1 {
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 1;
+
+
+        count += 1;
+        System.out.println("i = " + 1 + "count = " + count);
+        count += 2;
+        System.out.println("i = " + 2 + "count = " + count);
+        count += 3;
+        System.out.println("i = " + 3 + "count = " + count);
+    }
+}
